1e55485248db3cbc42e32f2d4ee8af08bf3407a0
Bowi3 <leonnardofender@gmail.com>
2024-09-06 13:35:49 -0300

    Add filter for JWT request authentication

--- src/main/java/com/personal/project/ecomerce_backend/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/personal/project/ecomerce_backend/api/controller/auth/AuthenticationController.java
@@ -2,10 +2,13 @@ package com.personal.project.ecomerce_backend.api.controller.auth;
 
 import com.personal.project.ecomerce_backend.api.model.*;
 import com.personal.project.ecomerce_backend.exception.UserAlreadyExistsException;
+import com.personal.project.ecomerce_backend.model.LocalUser;
 import com.personal.project.ecomerce_backend.service.UserService;
 import jakarta.validation.Valid;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -38,6 +41,11 @@ public class AuthenticationController {
             response.setJwt(jwt);
             return ResponseEntity.ok(response);
         }
+    }
+
+    @GetMapping("/me")
+    public LocalUser getLoggedInUserProfile(@AuthenticationPrincipal LocalUser user) {
+        return user;
     }
 
 }
