c75a8ade82680e9aba48a66d8c01459642de3575
Bowi3 <leonnardofender@gmail.com>
2024-09-06 10:43:18 -0300

    Add Login authentication with JWT token

--- src/main/java/com/personal/project/ecomerce_backend/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/personal/project/ecomerce_backend/api/controller/auth/AuthenticationController.java
@@ -4,7 +4,6 @@ import com.personal.project.ecomerce_backend.api.model.*;
 import com.personal.project.ecomerce_backend.exception.UserAlreadyExistsException;
 import com.personal.project.ecomerce_backend.service.UserService;
 import jakarta.validation.Valid;
-import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,6 +25,18 @@ public class AuthenticationController {
             return ResponseEntity.ok().build();
         } catch (UserAlreadyExistsException ex) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity loginUser(@Valid @RequestBody LoginBody loginBody) {
+        String jwt = userService.loginUser(loginBody);
+        if (jwt == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        } else {
+            LoginResponse response = new LoginResponse();
+            response.setJwt(jwt);
+            return ResponseEntity.ok(response);
         }
     }
 
--- src/main/java/com/personal/project/ecomerce_backend/service/UserService.java
+++ src/main/java/com/personal/project/ecomerce_backend/service/UserService.java
@@ -1,18 +1,25 @@
 package com.personal.project.ecomerce_backend.service;
 
+import com.personal.project.ecomerce_backend.api.model.LoginBody;
 import com.personal.project.ecomerce_backend.api.model.RegistrationBody;
 import com.personal.project.ecomerce_backend.exception.UserAlreadyExistsException;
 import com.personal.project.ecomerce_backend.model.LocalUser;
 import com.personal.project.ecomerce_backend.model.dao.LocalUserDAO;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     private LocalUserDAO localUserDAO;
+    private EncryptionService encryptionService;
+    private JWTService jwtService;
 
-    public UserService(LocalUserDAO localUserDAO) {
+    public UserService(LocalUserDAO localUserDAO, EncryptionService encryptionService, JWTService jwtService) {
         this.localUserDAO = localUserDAO;
+        this.encryptionService = encryptionService;
+        this.jwtService = jwtService;
     }
 
     public LocalUser registerUser(RegistrationBody registrationBody) throws UserAlreadyExistsException {
@@ -25,8 +32,18 @@ public class UserService {
         user.setUsername(registrationBody.getUsername());
         user.setFirstName(registrationBody.getFirstName());
         user.setLastName(registrationBody.getLastName());
-        //TODO: Encrypt passwords
-        user.setPassword(registrationBody.getPassword());
+        user.setPassword(encryptionService.encryptPassword(registrationBody.getPassword()));
         return localUserDAO.save(user);
+    }
+
+    public String loginUser(LoginBody loginBody) {
+        Optional<LocalUser> opUser = localUserDAO.findByUsernameIgnoreCase(loginBody.getUsername());
+        if (opUser.isPresent()) {
+            LocalUser user = opUser.get();
+            if (encryptionService.verifyPassword(loginBody.getPassword(), user.getPassword())) {
+                return jwtService.generateJWT(user);
+            }
+        }
+        return null;
     }
 }
