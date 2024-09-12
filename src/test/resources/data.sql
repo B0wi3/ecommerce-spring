b6563db524f053a49327f59510143db543ab352f
Bowi3 <leonnardofender@gmail.com>
2024-09-12 12:14:31 -0300

    Start testing UserService class

---
+++ src/test/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO local_user (email, first_name, last_name, password, username, email_verified)
+    VALUES ('UserA@junit.com', 'UserA-FirstName', 'UserA-LastName', '$2a$10$mO05LXc08XKDposFt.zC.u3sEW9q.6bzzApXyTBO/qN4/o/MJnOJW', 'UserA', true)
+    , ('UserB@junit.com', 'UserB-FirstName', 'UserB-LastName', '$2a$10$2m7yzAwEP2QHbUn/A4T6eOGWJ2i1YPpfDuDOu.JSv0R8vTcPgFeMO', 'UserB', false)
