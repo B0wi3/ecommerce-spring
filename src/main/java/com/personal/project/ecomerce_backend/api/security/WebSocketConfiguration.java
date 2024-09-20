7fd7f89d6eb1dd80a703251c4212a2f3b2e1f3bc
Bowi3 <leonnardofender@gmail.com>
2024-09-20 12:11:11 -0300

    Start configuring websockets and MQ

---
+++ src/main/java/com/personal/project/ecomerce_backend/api/security/WebSocketConfiguration.java
@@ -0,0 +1,35 @@
+package com.personal.project.ecomerce_backend.api.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocket
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket").setAllowedOriginPatterns("**").withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    private AuthorizationManager<Message<?>> makeMessageAuthorizationManager() {
+        MessageMatcherDelegatingAuthorizationManager.Builder messages =
+                new MessageMatcherDelegatingAuthorizationManager.Builder();
+
+    }
+
+}
