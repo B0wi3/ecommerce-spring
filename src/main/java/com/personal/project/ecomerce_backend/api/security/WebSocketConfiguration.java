b9e3dc730e3b329ad5ee4199a1824c95d8aa68ae
Bowi3 <leonnardofender@gmail.com>
2024-09-24 10:21:05 -0300

    More progress on websocket configuration and changes on JWTRequestFilter

--- src/main/java/com/personal/project/ecomerce_backend/api/security/WebSocketConfiguration.java
+++ src/main/java/com/personal/project/ecomerce_backend/api/security/WebSocketConfiguration.java
@@ -1,9 +1,14 @@
 package com.personal.project.ecomerce_backend.api.security;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authorization.AuthorizationEventPublisher;
 import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
 import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -14,6 +19,15 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocket
 @EnableWebSocketMessageBroker
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    private ApplicationContext context;
+
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSocketConfiguration(ApplicationContext context, JWTRequestFilter jwtRequestFilter) {
+        this.context = context;
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -29,7 +43,21 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     private AuthorizationManager<Message<?>> makeMessageAuthorizationManager() {
         MessageMatcherDelegatingAuthorizationManager.Builder messages =
                 new MessageMatcherDelegatingAuthorizationManager.Builder();
-
+        messages
+                .simpDestMatchers("/topic/user/**").authenticated()
+                .anyMessage().permitAll();
+        return messages.build();
     }
 
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        AuthorizationManager<Message<?>> authorizationManager =
+                makeMessageAuthorizationManager();
+        AuthorizationChannelInterceptor authInterceptor =
+                new AuthorizationChannelInterceptor(authorizationManager);
+        AuthorizationEventPublisher publisher =
+                new SpringAuthorizationEventPublisher(context);
+        authInterceptor.setAuthorizationEventPublisher(publisher);
+        registration.interceptors(jwtRequestFilter, authInterceptor);
+    }
 }
